5fd3211806219b85d6c3d58f03298e6178e7e865
Faten Sultan <1202750@student.birzeit.edu>
2024-02-27 17:03:30 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+  let name ="John Doe"
+  let age = "20"
+  return (
+    <>
+      <p>hello {name} </p>
+      <p> your age is {age}</p>
+    </>
+  )
+}
+
+export default App
